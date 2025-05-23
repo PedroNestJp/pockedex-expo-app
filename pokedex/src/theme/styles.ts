60032831a601095f043bbc705c1c122436cf5440
PedroNestJp <pedronestjp@gmail.com>
2025-05-23 00:35:06 -0300

    refactor: externalizar estilos para StyleSheet e remover inline styles

---
+++ pokedex/src/theme/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+});
